8e03d2cf8b4fab92c61986e110de5c32a2e66be1
wangyu_0928 <wangyu_0928@163.com>
2021-02-07 10:38:49 +0800

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     name='utils',
     version='0.0.1',
     description='base utils',
-    url='',
+    url='https://github.com/wangyu0928/Personal_Utils/tree/dev/utils',
     author='wangyu0928',
     author_email='wangyu_0928@163.com',
     license='MIT',
